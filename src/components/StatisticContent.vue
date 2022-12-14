75dece04ffcd406f6d9457089dea000a6e94c267
unknown <chelsafv75@gmail.com>
2022-12-14 23:51:03 +0700

    add statistic

---
+++ src/components/StatisticContent.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="container">
+        <section class="section-stats row justify-content-center" id="stats">
+            <div class="col-4 col-md-2 stats-detail">
+                <h2>200</h2>
+                <p>Customers</p>
+            </div>
+            <div class="col-4 col-md-2 stats-detail">
+                <h2>1000</h2>
+                <p>Task Done</p>
+            </div>
+            <div class="col-4 col-md-2 stats-detail">
+                <h2>75</h2>
+                <p>Teams</p>
+            </div>
+        </section>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'NavbarComponent',
+  components: {
+    
+  },
+}
+</script>
+
+<style>
+    .section-stats {
+        margin-top: -50px;
+    }
+    .section-stats .stats-detail {
+        padding-top: 15px;
+        background-color: #fff;
+        padding-left: 50px;
+    }
+    .section-stats h2 {
+        font-size: 30px;
+        margin-bottom: 0;
+    }
+    .section-stats p {
+        font-size: 20px;
+        font-weight: lighter;
+    }
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <NavbarComponent />
     <HeaderComponent />
+    <StatisticContent />
   </div>
 </template>
 
@@ -9,12 +10,20 @@
 // @ is an alias to /src
 import NavbarComponent from '@/components/NavbarComponent.vue'
 import HeaderComponent from '@/components/HeaderComponent.vue'
+import StatisticContent from '@/components/StatisticContent.vue';
 
 export default {
   name: 'HomeView',
   components: {
     NavbarComponent,
-    HeaderComponent
+    HeaderComponent,
+    StatisticContent
   }
 }
 </script>
+
+<style>
+    .home {
+        background-color: #FC9F9D;
+    }
+</style>
